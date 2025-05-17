f89e8273f42a0eeef180c468973c080a12449ecf
Harshraj4 <101204162+Harshraj4@users.noreply.github.com>
2025-05-17 20:45:16 +0530

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "10a5399b31ea98dece4b41d40eb15407";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
